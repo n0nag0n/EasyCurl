facc02016cd737991ac307971ac6fc3b46859a14
Austin Collier <acollier21@gmail.com>
2020-03-18 15:58:14 -0600

    added put and delete method and a few other if statements to round things out

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ Basic Usage
 	use n0nag0n\EasyCurl;
 
 	echo EasyCurl::getRequest('https://www.google.com/');
+
+	$result = EasyCurl::postRequest('https://example.com/v1/endpoint', [ 'is_json_request' => true, 'post_body' => '{"json":"values"}' ]);
 ```
 
 Check out the file on your own for documenation. It's pretty dang simple.
--- src/EasyCurl.php
+++ src/EasyCurl.php
@@ -14,6 +14,16 @@
 			$data['method'] = 'POST'; 
 			return self::makeCall($url, $data);
 		}
+
+		public static function deleteRequest($url, $data = []) {
+			$data['method'] = 'DELETE'; 
+			return self::makeCall($url, $data);
+		}
+
+		public static function putRequest($url, $data = []) {
+			$data['method'] = 'PUT'; 
+			return self::makeCall($url, $data);
+		}
 		
 		/**
 		 * Do the dang curl call
@@ -23,36 +33,65 @@
 		 */
 		private static function makeCall($url, $data = []) {
 			$headers = [];
-			if(!$data['method'])
+			if(!isset($data['method']) || empty($data['method'])) {
 				$data['method'] = 'GET';
-			
-			if($data['params'])
+			}
+
+			if(isset($data['params']) && $data['params']) {
 				$data['params'] = http_build_query($data['params']);
-			
+			}
 		    $curl = curl_init($url);
 		    curl_setopt($curl, CURLOPT_HEADER, 0);
 		    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $data['method']);
-			curl_setopt($curl, CURLOPT_TIMEOUT, 6);
 			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
 
-			if($data['is_json_request'])
-				$headers[] = 'Content-Type: application/json';
-
-			if($data['authorization'])
-				$headers[] = 'Authorization: '.$data['authorization'];
-			
-			if(strtoupper($data['method']) == 'POST') {
-			    curl_setopt($curl, CURLOPT_POST, 1);
-			    curl_setopt($curl, CURLOPT_POSTFIELDS, $data['params'].$data['post_body']);
+			if($data['time_out']) {
+				curl_setopt($curl, CURLOPT_TIMEOUT, $data['time_out']);
+			} else {
+				curl_setopt($curl, CURLOPT_TIMEOUT, 12);
 			}
-			if(count($headers))
-				curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-			if($data['ignore_ssl']) {
+			if(isset($data['http_referrer'])) {
+				curl_setopt($curl, CURLOPT_REFERER, $data['http_referrer']);
+			}
+
+			if(isset($data['userpwd']) && $data['userpwd']) {
+				curl_setopt($curl, CURLOPT_USERPWD, $data['userpwd']);
+			}
+
+			if(isset($data['is_json_request']) && $data['is_json_request']) {
+				$headers[] = 'Content-Type: application/json';
+			}
+
+			if(isset($data['accept_json']) && $data['accept_json']) {
+				$headers[] = 'Accept: application/json';
+			}
+
+			if(isset($data['authorization']) && $data['authorization']) {
+				$headers[] = 'Authorization: '.$data['authorization'];
+			}
+
+			if(isset($data['custom_headers']) && is_array($data['custom_headers']) && count($data['custom_headers'])) {
+				$headers[] = array_merge($headers, $data['custom_headers']);
+			}
+
+			if(strtoupper($data['method']) === 'POST' || strtoupper($data['method']) === 'PUT') {
+				curl_setopt($curl, CURLOPT_POST, 1);
+				// params_array_raw is when things should not be passed through http_build_query like CURLFile
+				$postfields = isset($data['params_array_raw']) && is_array($data['params_array_raw']) && count($data['params_array_raw']) ? $data['params_array_raw'] : $data['params'].$data['post_body'];
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
+			}
+
+			if(count($headers)) {
+				curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+			}
+
+			if(isset($data['ignore_ssl']) && $data['ignore_ssl']) {
 				curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
 				curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
 			}
+
 		    $response = curl_exec($curl);
 			
 			self::$last_request_info = curl_getinfo($curl);
@@ -60,7 +99,7 @@
 			$curl_errno = curl_errno($curl);
 			
 			if($curl_errno)
-				return Crap::message('cURL Error: "'.$curl_errno.' - '.$curl_error.'" cURL Info: '.print_r(curl_getinfo($curl), true));
+				throw new \Exception('cURL Error: "'.$curl_errno.' - '.$curl_error.'" cURL Info: '.print_r(curl_getinfo($curl), true));
 
 		    curl_close($curl);
 			
